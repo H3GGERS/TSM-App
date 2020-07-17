c9a8ebf83d8f0352ecb33e4a5e686711d0e9d19b
Kyle Hegge <mrhegge@gmail.com>
2020-07-17 13:13:02 -0600

    Logo Created

--- TSM App/TSM App/ContentView.swift
+++ TSM App/TSM App/ContentView.swift
@@ -10,12 +10,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Logo()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+
+
+struct Logo: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 5.0) {
+            VStack(alignment: .leading, spacing: -14.0) {
+                Text("TRADE")
+                    .font(.custom("Montserrat-Bold", size: 54))
+                
+                Text("SKILL")
+                    .font(.custom("Montserrat-Bold", size: 54))
+                
+                Text("MASTER")
+                    .font(.custom("Montserrat-Bold", size: 54))
+                    .frame(width: 327, alignment: .leading)
+            }
+            .frame(width: 327, height: 164)
+            VStack(alignment: .leading) {
+                Text("LEDGER")
+                    .font(.custom("Montserrat-Regular", size: 23))
+                    .multilineTextAlignment(.leading)
+                    .frame(width: 327, alignment: .leading)
+            }
+        }
     }
 }
