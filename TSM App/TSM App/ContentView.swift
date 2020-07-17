487ace4ead395b4cc2fa1b999b8b2440ad188b73
Kyle Hegge <mrhegge@gmail.com>
2020-07-17 14:38:31 -0600

    Added text input

--- TSM App/TSM App/ContentView.swift
+++ TSM App/TSM App/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Logo()
+            TextField(/*@START_MENU_TOKEN@*/"Email address"/*@END_MENU_TOKEN@*/, text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
         }
     }
 }
@@ -31,21 +32,24 @@ struct Logo: View {
             VStack(spacing: -14.0) {
                 Text("TRADE")
                     .font(.custom("Montserrat-Bold", size: 54))
+                    .foregroundColor(Color("Primary"))
                     .frame(width: 327, alignment: .leading)
                 
                 Text("SKILL")
                     .font(.custom("Montserrat-Bold", size: 54))
+                    .foregroundColor(Color("Primary"))
                     .frame(width: 327, alignment: .leading)
                 
                 Text("MASTER")
                     .font(.custom("Montserrat-Bold", size: 54))
+                    .foregroundColor(Color("Primary"))
                     .frame(width: 327, alignment: .leading)
             }
             .frame(width: 327, height: 164)
             VStack {
                 Text("LEDGER")
                     .font(.custom("Montserrat-Regular", size: 23))
-                    .multilineTextAlignment(.leading)
+                    .foregroundColor(Color("Primary"))
                     .frame(width: 327, alignment: .leading)
             }
         }
